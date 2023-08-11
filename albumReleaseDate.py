aa410295a757d1b3d6f99422fdf0712c2b3df658
Alexis A <alexis.agui17@gmail.com>
2023-08-10 23:57:49 -0500

    Album MBID Write to File finished
    Finished script that takes in a file of artist names and maps them to their corresponding MBID key.

--- albumReleaseDate.py
+++ albumReleaseDate.py
@@ -1,24 +1,29 @@
 import requests
 import json
 
+
 albumList = ['Fearless']
+albumResult = open("topArtistAlbumsMBID.txt", "w",encoding='utf-8')
 
-for albumName in albumList:
-    api_url = f'https://musicbrainz.org/ws/2/release/?query={albumName}&fmt=json'
+with open("topArtistAlbums.txt","r",encoding='utf-8') as albumFile:        
+    for albumName in albumFile:
+        api_url = f'https://musicbrainz.org/ws/2/release/?query={albumName}&fmt=json'
 
-    response = requests.get(api_url, headers={'User-Agent': 'YourApp/1.0'})
+        response = requests.get(api_url, headers={'User-Agent': 'albumMBIDmapper'})
 
-    if response.status_code == 200:
-        search_results = response.json()
-    else:
-        print(f"Request failed with status code {response.status_code}")
-    
-    # Extract MBID from search results
-    if 'releases' in search_results:
-        for release in search_results['releases']:
-            if 'id' in release:
-                mbid = release['id']
-                print(f"Album: {albumName}, MBID: {mbid}")
-                break
+        if response.status_code == 200:
+            search_results = response.json()
+        else:
+            print(f"Request failed with status code {response.status_code}")
+        
+        # Extract MBID from search results
+        if 'releases' in search_results:
+            for release in search_results['releases']:
+                if 'id' in release:
+                    mbid = release['id']
+                    print(f"Album: {albumName}, MBID: {mbid}")
+                    albumResult.write(f"Album: {albumName}, MBID: {mbid}"+'\n')
+                    break
+
 
 
